e5a8178f0c782e761736065d3d9dcaff637dc23c
Renan Gimenez <renangiimenez@gmail.com>
2026-01-10 23:51:40 -0300

    docs: implement swagger

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,38 @@
 import { fastify } from "fastify";
 import { fastifyCors } from "@fastify/cors";
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+
+import fastifySwagger from "@fastify/swagger";
+import fastifyApiReference from "@scalar/fastify-api-reference";
 
 import { getUsersRoute } from "./routes/get-users.route";
 
-export const app = fastify();
+export const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
   origin: "*",
+});
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Bun Fastify Deploy",
+      description: "Bun Fastify Deploy",
+      version: "1.0.0",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+app.register(fastifyApiReference, {
+  routePrefix: "/docs",
 });
 
 app.register(getUsersRoute, { prefix: "/users" });
--- src/routes/get-users.route.ts
+++ src/routes/get-users.route.ts
@@ -1,9 +1,55 @@
-import type { FastifyPluginAsync } from "fastify";
-import { makeUser, makeUsers } from "../mocks/user.factory";
+import { z } from "zod";
+import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 
-export const getUsersRoute: FastifyPluginAsync = async (app) => {
-  app.get("/", (request, reply) => {
-    const users = makeUsers(10);
-    reply.send(users);
-  });
+import { makeUsers } from "../mocks/user.factory";
+
+export const getUsersRoute: FastifyPluginAsyncZod = async (app) => {
+  app.get(
+    "/",
+    {
+      schema: {
+        tags: ["Users"],
+        response: {
+          200: z
+            .array(
+              z.object({
+                name: z.string(),
+                email: z.email(),
+                avatar: z.url(),
+              })
+            )
+            .describe("Usuários"),
+        },
+      },
+    },
+    (request, reply) => {
+      const users = makeUsers(10);
+      reply.send(users);
+    }
+  );
+
+  app.post(
+    "/",
+    {
+      schema: {
+        tags: ["Users"],
+        body: z.object({
+          name: z.string(),
+          email: z.email(),
+          avatar: z.url(),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { name, email, avatar } = request.body;
+
+      const newUser = {
+        name,
+        email,
+        avatar,
+      };
+
+      reply.send(newUser);
+    }
+  );
 };
